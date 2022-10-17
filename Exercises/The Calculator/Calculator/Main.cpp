b0ca5836f497119e23fdd6c72ca5f406a924e861
Karsten Vermeulen <Karsten.vermeulen.dev@gmail.com>
2022-10-17 13:38:54 +0100

    Improve calculator project

--- Exercises/The Calculator/Calculator/Main.cpp
+++ Exercises/The Calculator/Calculator/Main.cpp
@@ -4,11 +4,7 @@
 //Function for adding the numbers
 int Add(int numberOne, int numberTwo)
 {
-	int result = numberOne + numberTwo;
-	return result;
-
-	//Better way to do this
-	//return numberOne + numberTwo;
+	return numberOne + numberTwo;
 }
 
 //Function for subtracting one number from another
@@ -29,53 +25,93 @@ int Divide(int numberOne, int numberTwo)
 	return numberOne / numberTwo;
 }
 
-int main() 
+int main()
 {
-	system("cls");
+	bool isAppRunning = true;
 
-	auto firstNumber{ 0 };
-	auto secondNumber{ 0 };
-
-	std::cout << "Please enter your first number: ";
-	std::cin >> firstNumber;
-
-	std::cout << "Please enter your second number: ";
-	std::cin >> secondNumber;
-
-	char operation;
-	std::cout << "What would you like to do with the numbers ['+', '-', '*', '/']?";
-	std::cin >> operation;
-
-	int result = 0;
-
-	if (operation == '+')
+	while (isAppRunning) 
 	{
-		result = Add(firstNumber, secondNumber);
+		system("cls");
+		std::cout << "#=======================#" << std::endl;
+		std::cout << "|      CALCULATOR       |" << std::endl;
+		std::cout << "#=======================#" << std::endl;
+		std::cout << "| 1. Add                |" << std::endl;
+		std::cout << "| 2. Subtract           |" << std::endl;
+		std::cout << "| 3. Multiply           |" << std::endl;
+		std::cout << "| 4. Divide             |" << std::endl;
+		std::cout << "| 5. Exit               |" << std::endl;
+		std::cout << "#=======================#" << std::endl;
+
+		int choice = 0;
+		std::cin >> choice;
+
+		int result{ 0 };
+		int firstNumber{ 0 };
+		int secondNumber{ 0 };
+
+		if (choice == 1)
+		{
+			std::cout << "Please enter your first number: ";
+			std::cin >> firstNumber;
+
+			std::cout << "Please enter your second number: ";
+			std::cin >> secondNumber;
+
+			result = Add(firstNumber, secondNumber);
+			std::cout << "The result is " << result << std::endl;
+		}
+
+		else if (choice == 2)
+		{
+			std::cout << "Please enter your first number: ";
+			std::cin >> firstNumber;
+
+			std::cout << "Please enter your second number: ";
+			std::cin >> secondNumber;
+
+			result = Subtract(firstNumber, secondNumber);
+			std::cout << "The result is " << result << std::endl;
+		}
+
+		else if (choice == 3)
+		{
+			std::cout << "Please enter your first number: ";
+			std::cin >> firstNumber;
+
+			std::cout << "Please enter your second number: ";
+			std::cin >> secondNumber;
+
+			result = Multiply(firstNumber, secondNumber);
+			std::cout << "The result is " << result << std::endl;
+		}
+
+		else if (choice == 4)
+		{
+			std::cout << "Please enter your first number: ";
+			std::cin >> firstNumber;
+
+			std::cout << "Please enter your second number: ";
+			std::cin >> secondNumber;
+
+			if (secondNumber == 0)
+			{
+				std::cout << "Cannot divide by zero." << std::endl;
+			}
+
+			else
+			{
+				result = Divide(firstNumber, secondNumber);
+				std::cout << "The result is " << result << std::endl;
+			}
+		}
+
+		else if (choice == 5)
+		{
+			isAppRunning = false;
+		}
 	}
 
-	else if (operation == '-')
-	{
-		result = Subtract(firstNumber, secondNumber);
-	}
-
-	else if (operation == '*')
-	{
-		result = Multiply(firstNumber, secondNumber);
-	}
-
-	else if (operation == '/')
-	{
-		result = Divide(firstNumber, secondNumber);
-	}
-
-	std::cout << "The result is " << result << std::endl;
-
-	/*std::cout << "Adding the two numbers will yield " << firstNumber + secondNumber << std::endl;
-	std::cout << "Subtracting the two numbers will yield " << firstNumber - secondNumber << std::endl;
-	std::cout << "Multiplying the two numbers will yield " << firstNumber * secondNumber << std::endl;
-	std::cout << "Dividing the two numbers will yield " << firstNumber / secondNumber << std::endl;*/
-
-
+	std::cout << "Thanks for using the Calculator. Goodbye." << std::endl;
 	system("pause");
 	return 0;
 }
