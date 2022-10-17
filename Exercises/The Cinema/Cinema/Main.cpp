d73934c859e9c036e5b3f53655ae96e57e3cbb73
Karsten Vermeulen <Karsten.vermeulen.dev@gmail.com>
2022-10-17 16:01:00 +0100

    Add code (WIP)

--- Exercises/The Cinema/Cinema/Main.cpp
+++ Exercises/The Cinema/Cinema/Main.cpp
@@ -86,3 +86,57 @@ void main()
 //	std::cout << std::endl;
 //
 //}
+
+//=====================================================================================
+
+////declare global constants for the seating arrangement array
+//const int ROWS = 4;
+//const int SEATS = 4;
+//
+////declare global constants for the game map challenge
+//const int TOTAL_ROOMS = 6;
+//const int TOTAL_ITEMS = 5;
+//
+////*****************************************************************************
+////CHALLENGE #1
+////*****************************************************************************
+//
+////create a multidimensional array to store a seating arrangement
+////set all initial values of the array to 'A' for "Available" 
+//short row, seat;
+//char seating[ROWS][SEATS] = { { 'A', 'A', 'A', 'A' },
+//							  { 'A', 'A', 'A', 'A' },
+//							  { 'A', 'A', 'A', 'A' },
+//							  { 'A', 'A', 'A', 'A' } };
+//
+////user can enter their desired seat
+//cout << "Enter your desired row: ";
+//cin >> row;
+//cout << "Enter your desired column: ";
+//cin >> seat;
+//
+////set desired seat to 'R' for "Reserved" and subtract 1 from each value because the array is zero-based
+//seating[row - 1][seat - 1] = 'R';
+//
+////clear the terminal screen and display each row and seat value after the booking
+//system("cls");
+//cout << seating[0][0] << ' ' << seating[0][1] << ' ' << seating[0][2] << ' ' << seating[0][3] << endl;
+//cout << seating[1][0] << ' ' << seating[1][1] << ' ' << seating[1][2] << ' ' << seating[1][3] << endl;
+//cout << seating[2][0] << ' ' << seating[2][1] << ' ' << seating[2][2] << ' ' << seating[2][3] << endl;
+//cout << seating[3][0] << ' ' << seating[3][1] << ' ' << seating[3][2] << ' ' << seating[3][3] << endl << endl;
+//
+////UNCOMMENT TO DEMO USING FOR-LOOP (for those comfortable with them)
+//system("cls");
+//for (int i = 0; i < ROWS; i++)
+//{
+//
+//	for (int j = 0; j < SEATS; j++)
+//	{
+//		cout << seating[i][j] << ' ';
+//	}
+//
+//	cout << endl;
+//
+//}
+//
+//cout << endl;
